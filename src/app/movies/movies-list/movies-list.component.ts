b37a413320a50aa6bae7f2a6a137f467c2d430b3
kannanATexcelforte <kannan@excelforte.com>
2025-06-12 19:28:25 +0530

    12.06.2025
    completed

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -9,7 +9,18 @@ import { Component, Input } from '@angular/core';
 })
 export class MoviesListComponent {
   @Input({ required: true })
-  movies?: any[];
+  movies!: any[];
 
-  
+  addMovie() {
+    this.movies?.push({
+      title: 'Inception',
+      releaseDate: new Date('2012-07-16'),
+      price: 500,
+    });
+  }
+
+  removeMovie(movie: any) {
+    let index = this.movies.findIndex((m: any) => m.title === movie.title);
+      this.movies.splice(index!, 1);
+  }
 }

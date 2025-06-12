2241a86d9b3d095c7b8851df4cf1c0328f7fd4c3
kannanATexcelforte <kannan@excelforte.com>
2025-06-12 20:23:53 +0530

    12.06.2025
    completed

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,10 +1,12 @@
 import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { GenericListComponent } from '../../shared/components/generic-list/generic-list.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-movies-list',
-  imports: [UpperCasePipe, DatePipe, CurrencyPipe, GenericListComponent],
+  imports: [UpperCasePipe, DatePipe, CurrencyPipe, GenericListComponent, MatButtonModule, MatIconModule],
   templateUrl: './movies-list.component.html',
   styleUrl: './movies-list.component.css'
 })

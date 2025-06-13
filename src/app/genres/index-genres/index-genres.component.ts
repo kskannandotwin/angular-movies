d3d9b7405d93d7497e31dc6e1455490249f6c120
kannanATexcelforte <kannan@excelforte.com>
2025-06-13 14:07:44 +0530

    13.06.2025
    completed

--- src/app/genres/index-genres/index-genres.component.ts
+++ src/app/genres/index-genres/index-genres.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-index-genres',
-  imports: [],
+  imports: [RouterLink, MatIconModule, MatButtonModule],
   templateUrl: './index-genres.component.html',
   styleUrl: './index-genres.component.css'
 })

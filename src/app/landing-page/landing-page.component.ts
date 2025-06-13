e4484a601cc17b8cd318fa020d0c5e36b785b4b1
kannanATexcelforte <kannan@excelforte.com>
2025-06-13 13:10:31 +0530

    13.06.2025
    completed

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { MoviesListComponent } from '../movies/movies-list/movies-list.component';
+
+@Component({
+  selector: 'app-landing-page',
+  imports: [MoviesListComponent],
+  templateUrl: './landing-page.component.html',
+  styleUrl: './landing-page.component.css'
+})
+export class LandingPageComponent {
+  upcomingReleasesMovies!: any;
+  inTheatersMovies!: any;
+
+  constructor() {
+    setTimeout(() => {
+      this.upcomingReleasesMovies = [
+        {
+          title: 'Inside Out 2',
+          releaseDate: new Date,
+          price: 1400.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f7/Inside_Out_2_poster.jpg/250px-Inside_Out_2_poster.jpg'
+        },
+        {
+          title: 'Batman',
+          releaseDate: new Date(2016, 5, 3),
+          price: 1200.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/c/c7/Batman_Infobox.jpg'
+        },
+        {
+          title: 'Superman',
+          releaseDate: new Date(2012, 10, 11),
+          price: 1300.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png'
+        }
+      ];
+
+      this.inTheatersMovies = [
+        {
+          title: 'Iron Man',
+          releaseDate: new Date(2018, 11, 21),
+          price: 1500.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/47/Iron_Man_%28circa_2018%29.png/250px-Iron_Man_%28circa_2018%29.png'
+        },
+        {
+          title: 'Captain America',
+          releaseDate: new Date(2019, 4, 30),
+          price: 1600.99,
+          poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/bf/CaptainAmericaHughes.jpg/250px-CaptainAmericaHughes.jpg'
+        }
+      ]
+    }, 100);
+  }
+}

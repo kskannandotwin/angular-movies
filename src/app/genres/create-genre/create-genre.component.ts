79da49d63e296e13425437902d6b731a4fedde75
kannanATexcelforte <kannan@excelforte.com>
2025-06-13 18:29:12 +0530

    13.06.2025
    completed

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,17 +1,26 @@
 import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-genre',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
 export class CreateGenreComponent {
 
   router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    name: ['']
+  });
   saveChanges() {
+    console.log(this.form.value);
     this.router.navigate(['/genres']);
   }
 }

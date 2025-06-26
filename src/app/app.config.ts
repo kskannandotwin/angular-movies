3df535055726d32b83552b0ac46f95f4dff0b531
kannanATexcelforte <kannan@excelforte.com>
2025-06-26 12:03:41 +0530

    26.06.2025
    completed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { provideMomentDateAdapter } from '@angular/material-moment-adapter';
 
 import { routes } from './app.routes';
 
@@ -12,7 +13,18 @@ export const appConfig: ApplicationConfig = {
     provideAnimationsAsync(),
     {
       provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue: { subscriptSizing: 'dynamic' },
+      useValue: { subscriptSizing: 'dynamic' }
     },
+    provideMomentDateAdapter({
+      parse: {
+        dateInput: ['DD-MM-YYYY']
+      },
+      display: {
+        dateInput: 'DD-MM-YYYY',
+        monthYearLabel: 'MMM YYYY',
+        dateA11yLabel: 'LL',
+        monthYearA11yLabel: 'MMMM YYYY'
+      }
+    })
   ],
 };

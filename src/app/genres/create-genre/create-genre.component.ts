abfb55ae77ef41064525d6bf93806159975a1b48
kannanATexcelforte <kannan@excelforte.com>
2025-06-26 11:03:09 +0530

    26.06.2025
    completed

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -6,36 +6,20 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { first } from 'rxjs';
 import { firstLetterShouldBeUppercase } from '../../shared/functions/validations';
+import { GenreCreationDTO } from '../genres.models';
+import { GenresFormComponent } from '../genres-form/genres-form.component';
 
 @Component({
   selector: 'app-create-genre',
-  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, RouterLink],
+  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, GenresFormComponent],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
 export class CreateGenreComponent {
 
   router = inject(Router);
-  private formBuilder = inject(FormBuilder);
-
-  form = this.formBuilder.group({
-    name: ['', {
-      validators: [Validators.required, firstLetterShouldBeUppercase()],
-    }]
-  });
-
-  getErrorMessagesForName() {
-    let field = this.form.controls.name;
-    if (field.hasError('required')) {
-      return 'The name field is required.';
-    }
-    if (field.hasError('firstLetterShouldBeUppercase')) {
-      return field.getError('firstLetterShouldBeUppercase').message;
-    }
-    return '';
-  }
-  saveChanges() {
-    console.log(this.form.value);
+  saveChanges(genre: GenreCreationDTO) {
+    console.log(genre);
     this.router.navigate(['/genres']);
   }
 }
